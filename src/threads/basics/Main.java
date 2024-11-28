6ef2e67bd4e8b51381d5cf9dabd378384e5638e1
KRashmika13 <krashmika13@gmail.com>
2024-11-28 22:40:39 +0530

    Did some changes to main class

--- src/threads/basics/Main.java
+++ src/threads/basics/Main.java
@@ -1,4 +1,15 @@
 package threads.basics;
 
 public class Main {
+    public static void main(String[] args) {
+        Thread myThread = new MyThread();
+        myThread.start();
+
+        Runnable myRunnable = new MyRunnable();
+        Thread thread = new Thread(myRunnable);
+        thread.start();
+
+
+    }
+
 }
