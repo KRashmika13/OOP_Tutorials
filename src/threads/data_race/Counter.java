4d376e8d725bce9f73d3b7c9cad23dd078a4a3d3
KRashmika13 <krashmika13@gmail.com>
2024-11-28 23:02:35 +0530

    add some methods to counter class

--- src/threads/data_race/Counter.java
+++ src/threads/data_race/Counter.java
@@ -1,4 +1,17 @@
 package threads.data_race;
 
 public class Counter {
+    private int count;
+
+    public Counter(int initialCount) {
+        count = initialCount;
+    }
+
+    public void increment() {
+        count++;
+    }
+
+    public int getCount() {
+        return count;
+    }
 }
