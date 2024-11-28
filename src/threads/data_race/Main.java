56524c4a03c860b16501bbb6fa8eb6660c4602c2
KRashmika13 <krashmika13@gmail.com>
2024-11-28 23:18:56 +0530

    did some changes to java class

--- src/threads/data_race/Main.java
+++ src/threads/data_race/Main.java
@@ -1,4 +1,18 @@
 package threads.data_race;
 
 public class Main {
+
+    public static void main(String[] args) {
+        Counter counter = new Counter(0);
+
+        Incrementor incrementor = new Incrementor(counter);
+
+        Thread t1 = new Thread(incrementor);
+        Thread t2 = new Thread(incrementor);
+        Thread t3 = new Thread(incrementor);
+
+        t1.start();
+        t2.start();
+        t3.start();
+    }
 }
